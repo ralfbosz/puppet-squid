86787e6f63ae0d61a60c82ffcee2c51fb5f8e3d2
Ralf Bosz <ralf@bosz.com>
2017-06-30 14:31:40 +0200

    This commit adds a rspec for the extra_config_section when using an array

--- spec/defines/extra_config_section_spec.rb
+++ spec/defines/extra_config_section_spec.rb
@@ -54,6 +54,36 @@ describe 'squid::extra_config_section' do
           expect(content).to match(expected_config_section)
         end
       end
+      let(:title) { 'my array config section' }
+
+      expected_config_section  = %(# my array config section\n)
+      expected_config_section += %(refresh_pattern ^ftp:           1440    20%     10080\n)
+      expected_config_section += %(refresh_pattern ^gopher:        1440    0%      1440\n)
+      expected_config_section += %(refresh_pattern -i (/cgi-bin/|\?) 0     0%      0\n)
+      expected_config_section += %(refresh_pattern .               0       20%     4320\n)
+      expected_config_section += %(\n)
+
+      context 'when config entry parameters are array' do
+        let(:params) do
+          {
+            config_entries: {
+              'refresh_pattern' => [
+                '^ftp:           1440    20%     10080',
+                '^gopher:        1440    0%      1440',
+                '-i (/cgi-bin/|\?) 0     0%      0',
+                '.               0       20%     4320'
+              ],
+            }
+          }
+        end
+
+        it { is_expected.to contain_concat_fragment('squid_extra_config_section_my config section').with_target('/tmp/squid.conf') }
+        it { is_expected.to contain_concat_fragment('squid_extra_config_section_my config section').with_order('60-my config section') }
+        it 'config section' do
+          content = catalogue.resource('concat_fragment', 'squid_extra_config_section_my config section').send(:parameters)[:content]
+          expect(content).to match(expected_config_section)
+        end
+      end
     end
   end
 end
